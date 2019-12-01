f2d012a5dea0754418688d791d6b052847753b73
yihangx <yx141@duke.edu>
2019-12-01 16:24:00 -0500

    submit

--- task_scheduler.py
+++ task_scheduler.py
@@ -1,19 +1,7 @@
-nums = [2,0,2,1,1,0,2,1,0,1,1,0,0]
+tasks = ["A","A","A","B","B","B"]
+n = 2
 
-def sort_colors(nums):
-    red = 0
-    white = 0
-    blue = len(nums) - 1
-
-    while white <= blue:
-        if nums[white] == 0:
-            nums[red], nums[white] = nums[white], nums[red]
-            white +=1
-            red += 1
-        elif nums[white] == 1:
-            white += 1
-        else:
-            nums[blue], nums[white] = nums[white], nums[blue]
-            blue -= 1
-    return nums
-print(sort_colors(nums))
+class Solution:
+    def leastInterval(self, tasks):
+        c = list(map(tasks.count, set(tasks)))
+        return max(sum(c), (max(c) - 1) * (n + 1) + c.count(max(c)))
