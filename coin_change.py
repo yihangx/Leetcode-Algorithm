d604577f0e176d1543c5ea231b81ae2b610be348
yihangx <yx141@duke.edu>
2019-12-16 20:44:55 -0500

    update

---
+++ coin_change.py
@@ -0,0 +1,11 @@
+coins = [1,2,5]
+amount = 11
+
+def coinchange(coins, amount):
+    dp = [0] + [float('inf')] * amount
+    for coin in coins:
+        for i in range(coin, amount + 1):
+            dp[i] = min(dp[i], dp[i-coin]+1)
+    return dp[-1] if dp[-1] != float('inf') else -1
+
+print(coinchange(coins, amount))
