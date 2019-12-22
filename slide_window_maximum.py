2371dc9b82b0838a55f9d7276d113a853923f270
yihangx <yx141@duke.edu>
2019-12-22 16:29:41 -0500

    update

---
+++ slide_window_maximum.py
@@ -0,0 +1,18 @@
+nums = [1,3,-1,-3,5,3,6,7]
+k = 3
+import collections
+
+def slide_window(nums, k):
+    res = []
+    deq = collections.deque()
+    for index, value in enumerate(nums):
+        while deq and nums[deq[-1]] <= value:
+            deq.pop()
+        deq += [index]
+        if index - deq[0] >= k:
+            deq.popleft()
+        if index + 1 >= k:
+            res.append(nums[deq[0]])
+    return res
+
+print(slide_window(nums, k))
