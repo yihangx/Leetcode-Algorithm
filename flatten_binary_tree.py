e54b05bfe840207fbaa44105736ea6ee78905ef5
yihangx <yx141@duke.edu>
2019-12-30 18:22:35 -0500

    update

---
+++ flatten_binary_tree.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def flatten(self, root):
+        while root:
+            if root.left:
+                self.flatten(root.left)
+                tail = root.left
+                while tail.right:
+                    tail = tail.right
+                tail.right = root.right
+                root.right = root.left
+                root.left = None
+            root = root.right
